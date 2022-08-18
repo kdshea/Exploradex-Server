9021fde9e465f266b0a1f2a1d461588128649eaa
serhanmiah <serhanmiah@icloud.com>
2022-08-18 09:34:26 +0100

    added seeding data

--- utilities/seedingData.js
+++ utilities/seedingData.js
@@ -8,7 +8,8 @@ const countries = [
   },
   {
     name: 'Japan',
-    description: 'I want to visit this place',
+    description: 'Japan is one of the most popular travel destinations in the world. It is a unique blend of traditional and modern, with many temples and buildings from the past co-existing with modern achievements in architecture and technology. Visitors can be immersed in Japanese history and culture one day and get a glimpse of the future through technological developments the next',
+    rating: '5',
   }
 
 ]
