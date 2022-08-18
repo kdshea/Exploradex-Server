7a68f45b1b0cf1d7cf6bb7f1df01fcf53d016062
serhanmiah <serhanmiah@icloud.com>
2022-08-18 12:44:00 +0100

    changes to the router file

--- server.js
+++ server.js
@@ -5,9 +5,18 @@ import connectToDb from './utilities/database.js'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 
+import router from './router.js'
+
+
 const PORT = 4000
 
 const app = express()
+
+app.use(express.json())
+
+
+
+app.use(router)
 
 
 // ! MIDDLEWARE
@@ -21,65 +30,7 @@ app.get('/', (request, response, next) => {
   return response.status(200).send('API is working. Health Check')
 })
 
-// ? END POINT TO GET ALL COUNTRIES
-// app.get('/countries', async (request, response) => {
-//   const allCountries = await travelModel.find()
-//   return response.status(200).json(allCountries)
-// })
 
-// ? END POINT TO GET INDIVIDUAL COUNTRY
-// app.get('/countries/:countryId', async (request, response, next) => {
-//   const { countryId } = request.params
-//   console.log('id', countryId)
-//   const foundCountry = await travelModel.findById(countryId)
-//   console.log('country', foundCountry)
-//   return response.status(200).json(foundCountry)
-// })
-
-
-// ? END POINT TO ADD A COUNTRY
-// app.put('/countries', async (request, response) => {
-
-//   const { body: newCountry } = request
-//   console.log('newCountry', newCountry)
-//   try {
-//     const createdDocument = await destinationModel.create(newCountry)
-//     return response.status(200).json(createdDocument)
-//   } catch (error) {
-//     return response.status(500).json({ messages: 'Something went wrong', error })
-//   }
-
-// })
-
-// ? REGISTER
-
-app.post('/register', async(request, response) => {
-  const { body: newUser } = request
-  console.log('newUser', newUser)
-
-  const emailExists = await userModel.findOne({ email: newUser.email })
-  if (emailExists) {
-    return response.status(400).json({ message: 'User with this email address already exists' })
-  }
-
-  const userNameExists = await userModel.findOne({ userName: newUser.userName })
-  console.log('userNameExists', userNameExists)
-
-  if (userNameExists) {
-    return response.status(400).json({ message: 'User with this username already exists' })
-  }
-  if (newUser.password !== newUser.confirmPassword) {
-    return response.status(400).json({ message: 'Passwords don\'t match' })
-  }
-
-  const salt = await bcrypt.genSalt(10)
-  const hashedPassword = await bcrypt.hash(newUser.password, salt)
-  const createdUser = await userModel.create({
-    ...newUser,
-    password: hashedPassword,
-  })
-  return response.status(200).json({ createdUser })
-})
 
 // ? LOGIN
 
